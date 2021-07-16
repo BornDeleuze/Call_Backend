89ccdf8691d7d66aac388880af6cd2bc0084cfa4
Matt Beaudoin <ehxtwo@gmail.com>
2021-07-16 12:16:17 -0700

    adding serializer, channel, and restructuring database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :messages
+  # resources :conversations
+  resources :users, only: [:show, :create]
+
+  Rails.application.routes.draw do
+    resources :conversations, only: [:index, :create]
+    resources :messages, only: [:create]
+    mount ActionCable.server => '/cable'
+  end
+
 end
---
+++ db/migrate/20210714203058_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.text :content
+      t.string :user_id
+      t.string :conversation_id
+
+      t.timestamps
+    end
+  end
+end
