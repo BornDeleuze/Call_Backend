c5cc120f2b3106149c43a30e4110a12c594b0d22
Matt Beaudoin <ehxtwo@gmail.com>
2021-07-16 12:24:27 -0700

    adding action cable to controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,13 +4,7 @@ class ConversationsController < ApplicationController
   # GET /conversations
   def index
     @conversations = Conversation.all
-
     render json: @conversations
-  end
-
-  # GET /conversations/1
-  def show
-    render json: @conversation
   end
 
   # POST /conversations
@@ -18,34 +12,38 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.new(conversation_params)
 
     if @conversation.save
-      render json: @conversation, status: :created, location: @conversation
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        ConversationSerializer.new(conversation)
+      ).serializable_hash
+      ActionCable.server.broadcast 'conversations_channel', serialized_data
+      head :ok
     else
       render json: @conversation.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /conversations/1
-  def update
-    if @conversation.update(conversation_params)
-      render json: @conversation
-    else
-      render json: @conversation.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /conversations/1
+  # def update
+  #   if @conversation.update(conversation_params)
+  #     render json: @conversation
+  #   else
+  #     render json: @conversation.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /conversations/1
-  def destroy
-    @conversation.destroy
-  end
+  # # DELETE /conversations/1
+  # def destroy
+  #   @conversation.destroy
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_conversation
-      @conversation = Conversation.find(params[:id])
-    end
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_conversation
+    #   @conversation = Conversation.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     def conversation_params
-      params.fetch(:conversation, {})
+      params.fetch(:conversation).permit(:name)
     end
 end
